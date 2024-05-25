3ab99baecec52b593e84ffb1fbb6ceba1920e68c
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-05-26 00:35:16 +0600

    add student validations

---
+++ src/app/modules/student/student.validation.ts
@@ -0,0 +1,44 @@
+import { z } from 'zod';
+
+const userNameValidationSchema = z.object({
+    firstName: z.string({
+        required_error: 'First Name is required',
+    }),
+    middleName: z.string().optional(),
+    lastName: z.string(),
+});
+
+const guardianValidationSchema = z.object({
+    fatherName: z.string(),
+    fatherOccupation: z.string(),
+    fatherContactNo: z.string(),
+    motherName: z.string(),
+    motherOccupation: z.string(),
+    motherContactNo: z.string(),
+});
+
+const localGuardianValidationSchema = z.object({
+    name: z.string(),
+    occupation: z.string(),
+    contactNo: z.string(),
+    address: z.string(),
+});
+
+const studentValidationSchema = z.object({
+    name: userNameValidationSchema,
+    gender: z.enum(['male', 'female', 'other']),
+    dateOfBirth: z.string(), // Might want to validate this further as a date
+    email: z.string().email(),
+    contactNo: z.string(),
+    emergencyContactNo: z.string(),
+    presentAddress: z.string(),
+    permanentAddress: z.string(),
+    guardian: guardianValidationSchema,
+    localGuardian: localGuardianValidationSchema,
+    profileImage: z.string().url(),
+    admissionSemester: z.string(),
+});
+
+export const StudentValidations = {
+    studentValidationSchema,
+};
