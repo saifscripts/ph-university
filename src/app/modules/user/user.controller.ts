592f17361718a2b9e814dd0aca8ce5864ea07be7
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-05-26 00:36:21 +0600

    create and use catchAsync hof

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,30 +1,22 @@
-import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import { UserServices } from './user.service';
 
-const createStudent = async (
-    req: Request,
-    res: Response,
-    next: NextFunction,
-) => {
-    try {
-        const { password, student } = req.body;
+const createStudent = catchAsync(async (req, res) => {
+    const { password, student } = req.body;
 
-        const studentData = await UserServices.createStudentIntoDB(
-            password,
-            student,
-        );
+    const studentData = await UserServices.createStudentIntoDB(
+        password,
+        student,
+    );
 
-        sendResponse(res, {
-            statusCode: httpStatus.OK,
-            message: 'Student created successfully!',
-            data: studentData,
-        });
-    } catch (error) {
-        next(error);
-    }
-};
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        message: 'Student created successfully!',
+        data: studentData,
+    });
+});
 
 export const UserControllers = {
     createStudent,
