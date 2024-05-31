19e8ea9ee460c3d64d71a9669e11949fd2478c54
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-06-01 02:12:37 +0600

    update route index

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,12 +1,18 @@
 import express from 'express';
+import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.route';
+import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
 import { SemesterRoutes } from '../modules/semester/semester.route';
+import { StudentRoutes } from '../modules/student/student.route';
 import { UserRoutes } from '../modules/user/user.route';
 
 const router = express.Router();
 
 const routes = [
     { path: '/users', route: UserRoutes },
+    { path: '/students', route: StudentRoutes },
     { path: '/semesters', route: SemesterRoutes },
+    { path: '/academic-faculties', route: AcademicFacultyRoutes },
+    { path: '/academic-departments', route: AcademicDepartmentRoutes },
 ];
 
 routes.forEach((route) => router.use(route.path, route.route));
