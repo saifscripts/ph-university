35ef98bd20e9a1ccdbc38305b058dd9903e513f7
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-06-02 10:16:18 +1200

    14.2: error patterns

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,18 +1,33 @@
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler } from 'express';
 import httpStatus from 'http-status';
+import { ZodError } from 'zod';
 
-const globalErrorHandler = (
-    err: { statusCode: number; message: string; error?: object },
-    _req: Request,
-    res: Response,
-    _next: NextFunction,
-) => {
+type TErrorSources = {
+    path: string | number;
+    message: string;
+}[];
+
+const globalErrorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
     const statusCode = err.statusCode || httpStatus.INTERNAL_SERVER_ERROR;
     const message = err.message || 'Something went wrong!';
+    const errorSources: TErrorSources = [
+        {
+            path: '',
+            message: 'Something went wrong!',
+        },
+    ];
+
+    if (err instanceof ZodError) {
+        errorSources.push({
+            path: 'zod',
+            message: 'Error from Zod',
+        });
+    }
 
     return res.status(statusCode).json({
         success: false,
         message,
+        errorSources,
         error: err,
     });
 };
