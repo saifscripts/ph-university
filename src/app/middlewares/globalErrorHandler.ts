f219c22bd72380fe75616ffc4000b831afbf9614
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-06-03 05:49:10 +0600

    14.6: handle AppError and Error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -3,6 +3,7 @@ import httpStatus from 'http-status';
 import mongoose from 'mongoose';
 import { ZodError } from 'zod';
 import config from '../config';
+import AppError from '../errors/AppError';
 import handleCastError from '../errors/handleCastError';
 import handleDuplicateError from '../errors/handleDuplicateError';
 import handleValidationError from '../errors/handleValidationError';
@@ -10,8 +11,8 @@ import handleZodError from '../errors/handleZodError';
 import { IErrorSources } from '../interfaces/errors';
 
 const globalErrorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
-    let statusCode = err.statusCode || httpStatus.INTERNAL_SERVER_ERROR;
-    let message = err.message || 'Something went wrong!';
+    let statusCode: number = httpStatus.INTERNAL_SERVER_ERROR;
+    let message: string = 'Something went wrong!';
     let errorSources: IErrorSources = [
         {
             path: '',
@@ -39,13 +40,29 @@ const globalErrorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
         statusCode = formattedError.statusCode;
         message = formattedError.message;
         errorSources = formattedError.errorSources;
+    } else if (err instanceof AppError) {
+        statusCode = err.statusCode;
+        message = err.message;
+        errorSources = [
+            {
+                path: '',
+                message: err.message,
+            },
+        ];
+    } else if (err instanceof Error) {
+        message = err.message;
+        errorSources = [
+            {
+                path: '',
+                message: err.message,
+            },
+        ];
     }
 
     return res.status(statusCode).json({
         success: false,
         message,
         errorSources,
-        err,
         stack: config.NODE_ENV === 'development' ? err?.stack : null,
     });
 };
