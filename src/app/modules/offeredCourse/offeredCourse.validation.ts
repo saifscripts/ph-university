421ef30533ca4396f6b5e46a76a678dc9f7cd7cc
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-08-19 03:45:25 +0600

    fix offered-course and semester-registration validation

--- src/app/modules/offeredCourse/offeredCourse.validation.ts
+++ src/app/modules/offeredCourse/offeredCourse.validation.ts
@@ -12,48 +12,52 @@ const timeStringSchema = z.string().refine(
     },
 );
 
-export const createOfferedCourseValidationSchema = z
-    .object({
-        semesterRegistration: ObjectId,
-        academicFaculty: ObjectId,
-        academicDepartment: ObjectId,
-        course: ObjectId,
-        faculty: ObjectId,
-        maxCapacity: z.number(),
-        section: z.number(),
-        days: z.array(z.enum(Days)),
-        startTime: timeStringSchema,
-        endTime: timeStringSchema,
-    })
-    .refine(
-        (body) => {
-            const startTime = new Date(`1970-01-01T${body.startTime}`);
-            const endTime = new Date(`1970-01-01T${body.endTime}`);
-            return endTime > startTime;
-        },
-        {
-            message: 'Start time should be smaller then end time.',
-        },
-    );
+export const createOfferedCourseValidationSchema = z.object({
+    body: z
+        .object({
+            semesterRegistration: ObjectId,
+            academicFaculty: ObjectId,
+            academicDepartment: ObjectId,
+            course: ObjectId,
+            faculty: ObjectId,
+            maxCapacity: z.number(),
+            section: z.number(),
+            days: z.array(z.enum(Days)),
+            startTime: timeStringSchema,
+            endTime: timeStringSchema,
+        })
+        .refine(
+            (body) => {
+                const startTime = new Date(`1970-01-01T${body.startTime}`);
+                const endTime = new Date(`1970-01-01T${body.endTime}`);
+                return endTime > startTime;
+            },
+            {
+                message: 'Start time should be smaller then end time.',
+            },
+        ),
+});
 
-export const updateOfferedCourseValidationSchema = z
-    .object({
-        faculty: ObjectId,
-        maxCapacity: z.number(),
-        days: z.array(z.enum(Days)),
-        startTime: timeStringSchema,
-        endTime: timeStringSchema,
-    })
-    .refine(
-        (body) => {
-            const startTime = new Date(`1970-01-01T${body.startTime}`);
-            const endTime = new Date(`1970-01-01T${body.endTime}`);
-            return endTime > startTime;
-        },
-        {
-            message: 'Start time should be smaller then end time.',
-        },
-    );
+export const updateOfferedCourseValidationSchema = z.object({
+    body: z
+        .object({
+            faculty: ObjectId,
+            maxCapacity: z.number(),
+            days: z.array(z.enum(Days)),
+            startTime: timeStringSchema,
+            endTime: timeStringSchema,
+        })
+        .refine(
+            (body) => {
+                const startTime = new Date(`1970-01-01T${body.startTime}`);
+                const endTime = new Date(`1970-01-01T${body.endTime}`);
+                return endTime > startTime;
+            },
+            {
+                message: 'Start time should be smaller then end time.',
+            },
+        ),
+});
 
 export const OfferedCourseValidations = {
     createOfferedCourseValidationSchema,
--- src/app/modules/semesterRegistration/semesterRegistration.validation.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.validation.ts
@@ -3,30 +3,34 @@ import { z } from 'zod';
 import { SemesterRegistrationStatus } from './semesterRegistration.constant';
 
 export const createSemesterRegistrationValidationSchema = z.object({
-    semester: z
-        .string()
-        .refine((value) => mongoose.Types.ObjectId.isValid(value), {
-            message: 'Invalid ObjectId',
-        }),
-    status: z.enum(SemesterRegistrationStatus),
-    startDate: z.string().datetime(),
-    endDate: z.string().datetime(),
-    minCredit: z.number().int().min(3).max(15),
-    maxCredit: z.number().int().min(3).max(15),
+    body: z.object({
+        semester: z
+            .string()
+            .refine((value) => mongoose.Types.ObjectId.isValid(value), {
+                message: 'Invalid ObjectId',
+            }),
+        status: z.enum(SemesterRegistrationStatus),
+        startDate: z.string().datetime(),
+        endDate: z.string().datetime(),
+        minCredit: z.number().int().min(3).max(15),
+        maxCredit: z.number().int().min(3).max(15),
+    }),
 });
 
 export const updateSemesterRegistrationValidationSchema = z.object({
-    semester: z
-        .string()
-        .refine((value) => mongoose.Types.ObjectId.isValid(value), {
-            message: 'Invalid ObjectId',
-        })
-        .optional(),
-    status: z.enum(SemesterRegistrationStatus).optional(),
-    startDate: z.string().datetime().optional(),
-    endDate: z.string().datetime().optional(),
-    minCredit: z.number().int().min(3).max(15).optional(),
-    maxCredit: z.number().int().min(3).max(15).optional(),
+    body: z.object({
+        semester: z
+            .string()
+            .refine((value) => mongoose.Types.ObjectId.isValid(value), {
+                message: 'Invalid ObjectId',
+            })
+            .optional(),
+        status: z.enum(SemesterRegistrationStatus).optional(),
+        startDate: z.string().datetime().optional(),
+        endDate: z.string().datetime().optional(),
+        minCredit: z.number().int().min(3).max(15).optional(),
+        maxCredit: z.number().int().min(3).max(15).optional(),
+    }),
 });
 
 export const SemesterRegistrationValidations = {
